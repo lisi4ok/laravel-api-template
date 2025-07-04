734a75e8a59b27c899dd0417da5af7a16b947a70
Zaio Klepoyshkov <lisi4ok@gmail.com>
2025-07-04 05:22:12 +0300

    Add paint no empty comments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
@@ -21,6 +21,6 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
     }
 }
